b6cd038738eebec73b09a56b066f2e71892c8ccb
haonhat0703 <haonhat2729@gmail.com>
2022-09-09 22:53:09 +0700

    threading

--- server.py
+++ server.py
@@ -1,26 +1,5 @@
 import socket
-
-HOST = '127.0.0.1'
-SERVER_ROOT = 65432
-FORMAT = 'utf8'
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# SOCK_STREAM
-s.bind((HOST, SERVER_ROOT))
-s.listen()
-
-print('SERVER SIDE')
-print('server: ', HOST, SERVER_ROOT)
-print('Waiting for Client')
-
-conn, addr = s.accept()  # wait client connect
-# conn nhan va trao doi duong truyen
-# addr lay dia chi client
-
-# with every client side
-print('client address: ', addr)
-print('conn:', conn.getsockname())
-# []
+import threading
 
 
 def recieveList(conn):
@@ -36,7 +15,11 @@ def recieveList(conn):
     return list
 
 
-try:
+def handleClient(conn, addr):
+    # with every client side
+    print('client address: ', addr)
+    print('conn:', conn.getsockname())
+    # []
 
     msg = 'none'
     while msg != 'x':
@@ -45,6 +28,39 @@ try:
         if(msg == 'list'):
             print('list: ', recieveList(conn))
 
-except:
+    print('client', addr, 'finished, closed')
+    conn.close()
 
-    print('error')
+
+HOST = '127.0.0.1'
+SERVER_ROOT = 65432
+FORMAT = 'utf8'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# SOCK_STREAM
+s.bind((HOST, SERVER_ROOT))
+s.listen()
+
+print('SERVER SIDE')
+print('server: ', HOST, SERVER_ROOT)
+print('Waiting for Client')
+
+nClient = 0
+while nClient < 3:
+
+    try:
+        conn, addr = s.accept()  # wait client connect
+        # conn nhan va trao doi duong truyen
+        # addr lay dia chi client
+        thr = threading.Thread(target=handleClient, args=(conn, addr))
+        thr.daemon = True  # kill thr
+        thr.start()
+
+    except:
+
+        print('error')
+
+    nClient += 1
+
+
+s.close()
