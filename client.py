7202dcd50039e58dbcac41db812a2f550961a54f
haonhat0703 <haonhat2729@gmail.com>
2022-09-09 20:37:09 +0700

    send data to server with sendall with encode

--- client.py
+++ client.py
@@ -10,3 +10,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('CLIENT SIDE')
 client.connect((HOST, SERVER_PORT))
 print('client address:', client.getsockname())
+
+
+username = input("username: ")
+
+client.sendall(username.encode(FORMAT))
+# gui du lien bind du lieu khi gui
