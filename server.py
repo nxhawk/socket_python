734c7d9e2d9a75ef6315d1fa8cc5b6847ba4c5b6
haonhat0703 <haonhat2729@gmail.com>
2022-09-09 20:19:59 +0700

    server socket

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '127.0.0.1'
+SERVER_ROOT = 65432
+FORMAT = 'utf8'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# SOCK_STREAM
+s.bind((HOST, SERVER_ROOT))
+s.listen()
+
+print('SERVER SIDE')
+print('server: ', HOST, SERVER_ROOT)
+print('Waiting for Client')
+
+conn, addr = s.accept()
